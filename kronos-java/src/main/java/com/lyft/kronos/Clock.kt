48af09967625fa653670ac20877ec8025b872615
Amelia Riely <amelia.riely@gmail.com>
2019-06-10 15:54:00 -0400

    More intelligible

--- kronos-java/src/main/java/com/lyft/kronos/Clock.kt
+++ kronos-java/src/main/java/com/lyft/kronos/Clock.kt
@@ -30,7 +30,7 @@ interface KronosClock : Clock {
      * Return the number of milliseconds that have elapsed since 00:00:00 Coordinated Universal Time (UTC), Thursday, 1 January 1970.
      *
      * Note that calling this method will trigger [syncInBackground] being called when necessary.
-     * You still call [sync]/[syncInBackground] once to ensure you have the correct
+     * You should still call [sync]/[syncInBackground] once to ensure you have the correct time
      * as soon as possible.
      *
      * @return the current time in milliseconds.
@@ -45,14 +45,12 @@ interface KronosClock : Clock {
     fun getCurrentNtpTimeMs(): Long?
 
     /**
-     * Same as [getCurrentTimeMs], but returned [KronosTime] object includes time since last NTP sync, or null if the time is coming from the fallback clock.
-     *
      * @return [KronosTime] for the current time
      */
     fun getCurrentTime(): KronosTime
 
     /**
-     * Synchronize time an NTP server.
+     * Synchronize time with an NTP server.
      *
      * @return true on the first successful response, false if no successful response.
      */
@@ -64,9 +62,8 @@ interface KronosClock : Clock {
     fun syncInBackground()
 
     /**
-     * You can call [shutdown] when you no longer need this service. This will shutdown
-     * the thread that perform sync in the background. Any subsequent call to [currentTime]
-     * will throw [IllegalStateException].
+     * Shuts down the thread that performs syncing in the background. Any subsequent call to [currentTime]
+     * will throw [IllegalStateException]. You can call [shutdown] when you no longer need this service.
      */
     fun shutdown()
 }
