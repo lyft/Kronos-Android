d23b9691b0a018675e3074474adfce2d64558d7a
Artur Dryomov <artur.dryomov@gmail.com>
2021-03-25 08:01:42 +0300

    Add debug info

--- kronos-java/src/main/java/com/lyft/kronos/sntp/Dns.kt
+++ kronos-java/src/main/java/com/lyft/kronos/sntp/Dns.kt
@@ -10,7 +10,9 @@ internal interface Dns {
     class Impl : Dns {
 
         override fun resolve(host: String) = try {
-            InetAddress.getAllByName(host).orEmpty()
+            InetAddress.getAllByName(host).orEmpty().apply {
+                println("IPs: ${this.map { it.hostAddress }}")
+            }
         } catch (e: SecurityException) {
             emptyArray()
         } catch (e: UnknownHostException) {
